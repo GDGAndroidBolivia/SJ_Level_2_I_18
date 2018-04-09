742c4683122f925d8a9f1947cbf6a785aecd995c
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-04-09 10:51:47 -0400

    Codigo base demo dialogos

--- SJ_L2_06_I_2018_Dialogos/app/src/main/java/com/miramicodigo/dialogos/MainActivity.java
+++ SJ_L2_06_I_2018_Dialogos/app/src/main/java/com/miramicodigo/dialogos/MainActivity.java
@@ -72,149 +72,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public AlertDialog crearDialogoSimple() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-
-        alertDialog.setTitle("Mi Dialogo")
-                .setMessage("Este es el contenido de mi dialogo")
-                .setCancelable(false)
-                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Aceptar",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Cancelar",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNeutralButton("Cerrar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Cerrar",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-        return alertDialog.create();
+        return null;
     }
 
 
     public AlertDialog crearDialogoConLista() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        final String[] opciones = {"Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4", "Opcion 5", "Opcion 6", "Opcion 7", "Opcion 8"};
-        alertDialog.setTitle("Elija una opcion")
-                .setCancelable(false)
-                .setItems(opciones, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(),
-                                "Hizo click en: "+ opciones[i], Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-        return alertDialog.create();
+        return null;
     }
 
     public AlertDialog crearDialogoConListaRadio() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        final String [] opciones = {"Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4"};
-        posicion = 0;
-        alertDialog.setTitle("Seleccione una opcion")
-                .setSingleChoiceItems(opciones, 0, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        posicion = i;
-                    }
-                })
-                .setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(),
-                                "Seleccionaste: "+ opciones[posicion], Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-        return alertDialog.create();
+        return null;
     }
 
     public AlertDialog crearDialogoCheckBox() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        final String[] opciones = {"Desarrollo Android", "Desarrollo iOS", "Back-end", "Front-end"};
-        final ArrayList<Integer> itemsSeleccionados = new ArrayList<Integer>();
-
-        alertDialog.setTitle("Seleccione sus Skills")
-                .setMultiChoiceItems(opciones, null, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
-                        if(b) {
-                            itemsSeleccionados.add(i);
-                        } else {
-                            if(itemsSeleccionados.contains(i)) {
-                                itemsSeleccionados.remove(Integer.valueOf(i));
-                            }
-                        }
-                    }
-                })
-                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String res = "";
-                        for (int j = 0; j < itemsSeleccionados.size(); j++) {
-                            res = res +"\n- "+opciones[itemsSeleccionados.get(j)];
-                        }
-                        Toast.makeText(getApplicationContext(), res, Toast.LENGTH_LONG).show();
-                    }
-                });
-        return alertDialog.create();
+        return null;
     }
 
     public AlertDialog crearDialogoPersonalizado() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setCancelable(false);
-        LayoutInflater inflater = getLayoutInflater();
-        View v = inflater.inflate(R.layout.custom_dialog, null);
-        alertDialog.setView(v);
-
-        btnCrear = v.findViewById(R.id.btnCrear);
-        btnEntrar = v.findViewById(R.id.btnEntrar);
-        etNombre = v.findViewById(R.id.etNombre);
-        etContrasenia = v.findViewById(R.id.etContrasenia);
-        cbRecordar = v.findViewById(R.id.cbRecordar);
-
-        final AlertDialog dialog = alertDialog.show();
-
-        btnCrear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        res = "";
-        btnEntrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                res = "Nombre: "+etNombre.getText().toString()+
-                        "\nRecordar: "+cbRecordar.isChecked();
-                Toast.makeText(getApplicationContext(), res, Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-            }
-        });
-
-        return alertDialog.create();
+        return null;
     }
 
 }
