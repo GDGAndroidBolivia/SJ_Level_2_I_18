8c2bf15a217cdf5588785c15b358e6ca78322432
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-04-17 23:26:35 -0400

    Demo menus: opciones, contextuales y popups

--- SJ_L2_09_I_2018_Menus/app/src/main/java/com/miramicodigo/menus/MainActivity.java
+++ SJ_L2_09_I_2018_Menus/app/src/main/java/com/miramicodigo/menus/MainActivity.java
@@ -1,7 +1,8 @@
 package com.miramicodigo.menus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,6 +24,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private List<String> datos;
     private ArrayAdapter<String> adapter;
 
+    // Menu de opciones -> Toolbar.
+    // Menu contextuales -> Se posicionan al centro de la pantalla o Toolbar.
+    // Menu Popup -> Se posiciona de acuerdo al elemento que se hizo click.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, datos);
         lvDatos.setAdapter(adapter);
 
+        registerForContextMenu(lvDatos);
+
     }
 
     public void llenarDatos() {
@@ -47,7 +54,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        final PopupMenu popupMenu = new PopupMenu(getApplicationContext(), btnShowPopup);
+        popupMenu.getMenuInflater().inflate(R.menu.menu_popup, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.menuPopUno:
+                        Toast.makeText(getApplicationContext(), "Popup 1", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menuPopDos:
+                        Toast.makeText(getApplicationContext(), "Popup 2", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menuPopTres:
+                        Toast.makeText(getApplicationContext(), "Popup 3", Toast.LENGTH_SHORT).show();
+                        return true;
+                        default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuOpUno:
+                Toast.makeText(getApplicationContext(),
+                        "Menu uno", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menuOpDos:
+                Toast.makeText(getApplicationContext(),
+                        "Menu dos", Toast.LENGTH_LONG).show();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_contextual, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuConUno:
+                Toast.makeText(getApplicationContext(), "Opc 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuConDos:
+                Toast.makeText(getApplicationContext(), "Opc 2", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuConTres:
+                Toast.makeText(getApplicationContext(), "Opc 3", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuConCuatro:
+                Toast.makeText(getApplicationContext(), "Opc 4", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
 }
