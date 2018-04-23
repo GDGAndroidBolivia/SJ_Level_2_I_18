8e04479fb508021a6ecc5649f5ede7e3b6949ee9
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-04-23 10:43:46 -0400

    Codigo base demo BottomNavigationView

--- SJ_L2_17_I_2018_BottomNavigationView/app/src/main/java/com/miramicodigo/bottomnavigationview/MainActivity.java
+++ SJ_L2_17_I_2018_BottomNavigationView/app/src/main/java/com/miramicodigo/bottomnavigationview/MainActivity.java
@@ -21,33 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
 
-        fragmentManager = getSupportFragmentManager();
 
-        transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frameContent, new MenuUnoFragment()).commit();
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.menu_1:
-                        fragment = new MenuUnoFragment();
-                        break;
-                    case R.id.menu_2:
-                        fragment = new MenuDosFragment();
-                        break;
-                    case R.id.menu_3:
-                        fragment = new MenuTresFragment();
-                        break;
-                }
-                transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.frameContent, fragment).commit();
-                return true;
-            }
-        });
     }
 
 }
