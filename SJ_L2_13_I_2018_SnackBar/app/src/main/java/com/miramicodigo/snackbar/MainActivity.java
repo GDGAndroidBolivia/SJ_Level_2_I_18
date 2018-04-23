c759f4caba9c93805279c51443292b9fd32df195
Gustavo Lizárraga <lizarraga@MiraMiCodigo.local>
2018-04-23 10:28:46 -0400

    Codigo base demo Snackbar

--- SJ_L2_13_I_2018_SnackBar/app/src/main/java/com/miramicodigo/snackbar/MainActivity.java
+++ SJ_L2_13_I_2018_SnackBar/app/src/main/java/com/miramicodigo/snackbar/MainActivity.java
@@ -31,48 +31,27 @@ public class MainActivity extends AppCompatActivity {
         btnSnackbarSimple.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar snackbar = Snackbar.make(coordinatorLayout,
-                        "Mensaje", Snackbar.LENGTH_SHORT);
-                snackbar.show();
+
+
+
             }
         });
 
         btnSnackbarConAccion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar snackbar = Snackbar.make(coordinatorLayout,
-                        "Mensaje eliminado", Snackbar.LENGTH_LONG)
-                        .setAction("Deshacer", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Toast.makeText(getApplicationContext(),
-                                        "OK", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                snackbar.show();
+
+
+
             }
         });
 
         btnSnackbarColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar snackbar = Snackbar.make(coordinatorLayout,
-                        "Mensaje eliminado", Snackbar.LENGTH_LONG)
-                        .setAction("Deshacer", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Toast.makeText(getApplicationContext(),
-                                        "OK", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                snackbar.setActionTextColor(Color.GREEN);
 
-                View sbView = snackbar.getView();
-                TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
-                textView.setTextColor(Color.BLUE);
-                sbView.setBackgroundColor(Color.YELLOW);
 
-                snackbar.show();
+
             }
         });
 
