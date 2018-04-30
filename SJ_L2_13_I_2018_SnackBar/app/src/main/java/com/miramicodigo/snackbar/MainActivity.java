2ffe5984d063aebb310f500d1d637bea6f2f2965
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-04-30 16:45:29 -0400

    Demo uso de SnackBar

--- SJ_L2_13_I_2018_SnackBar/app/src/main/java/com/miramicodigo/snackbar/MainActivity.java
+++ SJ_L2_13_I_2018_SnackBar/app/src/main/java/com/miramicodigo/snackbar/MainActivity.java
@@ -31,27 +31,46 @@ public class MainActivity extends AppCompatActivity {
         btnSnackbarSimple.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
+                Snackbar snackbar = Snackbar.make(coordinatorLayout,
+                        "Snackbak Simple", Snackbar.LENGTH_SHORT);
+                snackbar.show();
             }
         });
 
         btnSnackbarConAccion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(coordinatorLayout,
+                        "No hay conexion a internet", Snackbar.LENGTH_SHORT);
+                snackbar.setAction("Reintentar", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
+                    }
+                });
 
-
+                snackbar.show();
             }
         });
 
         btnSnackbarColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(coordinatorLayout,
+                        "Snackbar personalizado", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Accion", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
 
+                    }
+                });
+                snackbar.setActionTextColor(Color.BLUE);
+                View sb = snackbar.getView();
+                TextView tv = sb.findViewById(android.support.design.R.id.snackbar_text);
+                tv.setTextColor(Color.RED);
+                sb.setBackgroundColor(Color.GREEN);
 
-
+                snackbar.show();
             }
         });
 
