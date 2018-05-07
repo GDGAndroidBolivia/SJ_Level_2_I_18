0d42f0f222eea7fa3db20971e711f0d489226821
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-05-07 10:36:05 -0400

    Codigo base demo SQLite

---
+++ SJ_L2_18_I_2018_SQLite/app/src/main/java/com/miramicodigo/sqlite/FormularioActivity.java
@@ -0,0 +1,84 @@
+package com.miramicodigo.sqlite;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.miramicodigo.sqlite.db.DatabaseAdapter;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private DatabaseAdapter db;
+
+    private EditText etNombre;
+    private EditText etTelefono;
+    private EditText etCorreo;
+    private RadioButton rbMasculino;
+    private RadioButton rbFemenino;
+
+    private boolean edicion;
+
+    private long id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        etTelefono = (EditText) findViewById(R.id.etTelefono);
+        etCorreo = (EditText) findViewById(R.id.etCorreo);
+        rbMasculino = (RadioButton) findViewById(R.id.rbMasculino);
+        rbFemenino = (RadioButton) findViewById(R.id.rbFemenino);
+
+
+
+        if (getIntent().getExtras() != null) {
+            edicion = true;
+            setTitle("Editar persona");
+            id = getIntent().getLongExtra("id", 0);
+            Cursor cursor = db.obtenerPersona(id);
+            if(cursor.moveToFirst()) {
+
+            }
+        } else {
+            edicion = false;
+            setTitle("Nueva persona");
+        }
+
+    }
+
+    public void aceptar(View view) {
+        if(etNombre.getText().toString().equals("") ||
+                etCorreo.getText().toString().equals("") ||
+                etTelefono.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Hay campos vacios", Toast.LENGTH_LONG).show();
+        } else {
+            String nombre = etNombre.getText().toString();
+            long telefono = Long.parseLong(etTelefono.getText().toString());
+            String correo = etCorreo.getText().toString();
+            String genero = rbMasculino.isChecked() ? "m" : "f";
+            if (edicion) {
+
+            } else {
+
+            }
+            finish();
+        }
+    }
+
+    public void cancelar(View view) {
+        finish();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+}
